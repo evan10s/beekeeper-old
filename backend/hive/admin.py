f36b93a196cec77c2aa95fd5a60aa056bd804415
Evan Strat <evan10s@users.noreply.github.com>
2017-09-11 01:08:22 -0400

    Update to show models in admin

--- backend/hive/admin.py
+++ backend/hive/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from django.conf import settings
 # Register your models here.
+from .models import Treatment, BG, Dose, Extended_Bolus, Meal
+
+class BgInline(admin.TabularInline):
+    model = BG
+    extra = 1
+
+class DoseInline(admin.TabularInline):
+    model = Dose
+    extra = 1
+
+class MealInline(admin.TabularInline):
+    model = Meal
+    extra = 1
+
+class TreatmentAdmin(admin.ModelAdmin):
+    fields = ['owner']
+    inlines = [BgInline, DoseInline, MealInline]
+
+admin.site.register(Treatment,TreatmentAdmin)
